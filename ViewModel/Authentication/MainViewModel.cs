10e0ca24ac5f1c1f490732dd8789682480372517
Famouse <famouse_ru@mail.ru>
2021-10-17 18:20:52 +0300

    Applying the MVVM pattern. For Authentification structure. Add Commands for swap controls

---
+++ ViewModel/Authentication/MainViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using FeBinder.Authentication.ViewModel.Base;
+using FeBinder.Controls.Authentication;
+using FeBinder.ViewModel.Base;
+
+namespace FeBinder.ViewModel.Authentication
+{
+    class MainViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// Link to exits control for display
+        /// </summary>
+        private UserControl _currentControl;
+        /// <summary>
+        /// Login Control xaml
+        /// </summary>
+        private UserControl _loginControl;
+        /// <summary>
+        /// Register Control xaml
+        /// </summary>
+        private UserControl _registerControl;
+
+        private RelayCommand _displayLoginCommand;
+        private RelayCommand _displayRegisterCommand;
+
+        public RelayCommand DisplayLoginCommand
+        {
+            get
+            {
+                return _displayLoginCommand ??
+                       (_displayLoginCommand = new RelayCommand(obj =>
+                       {
+                           
+                           CurrentControl = _loginControl;
+
+                       }));
+            }
+        }
+
+        public RelayCommand DisplayRegisterCommand
+        {
+            get
+            {
+                return _displayRegisterCommand ??
+                       (_displayRegisterCommand = new RelayCommand(obj =>
+                       {
+                           CurrentControl = _registerControl;
+                       }));
+            }
+        }
+
+
+
+        public UserControl CurrentControl
+        {
+            get => _currentControl;
+            set
+            {
+                _currentControl = null;
+                _currentControl = value;
+                OnPropertyChanged("CurrentControl");
+            }
+        }
+
+        public MainViewModel()
+        {
+            _loginControl = new LoginControl();
+            _registerControl = new RegisterControl();
+
+            _currentControl = _loginControl;
+        }
+    }
+}
